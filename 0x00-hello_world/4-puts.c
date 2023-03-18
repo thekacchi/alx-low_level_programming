211836b4c57e15bce5475a09c80fefd31e66b138
Eze Onyedikachi <thekacchi1@gmail.com>
2023-03-17 18:07:32 -0700

    lol

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ * with a double quote in the beginning.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n);
+	return (0);
+}
