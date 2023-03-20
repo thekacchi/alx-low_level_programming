d379086f109604df507e7caa39fcb2a24bcf9bb9
Eze Onyedikachi <thekacchi1@gmail.com>
2023-03-20 15:01:57 -0700

    lol

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description : Prints alphabets a-z and newline
+  *Return: always 0
+  */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
