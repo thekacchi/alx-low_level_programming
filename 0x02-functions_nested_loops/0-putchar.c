d5a2ee6896f7253454f76aa67761d53142a72d09
Eze Onyedikachi <thekacchi1@gmail.com>
2023-03-23 15:36:34 -0700

    lol

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: prints pitchar
+ *Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Ihe emebiwo, Nested Loops
