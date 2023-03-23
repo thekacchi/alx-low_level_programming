9ce2301d2f8699c876ac833646e31d09c12c3310
Eze Onyedikachi <thekacchi1@gmail.com>
2023-03-23 16:06:46 -0700

    dhjd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabe
+ * description - prints alphabetsp
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char a;
+	for(a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
