ba1b288e16430b2b5d1fbca03b2d94a93626daf6
Eze Onyedikachi <thekacchi1@gmail.com>
2023-03-25 17:23:33 -0700

    lol

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_last_digit - prints the last digit
+  *
+  * @a: parameter to be printed
+  *
+  * Return: always 0
+  */
+int print_last_digit(int a)
+{
+	int b;
+
+	b = (a % 10);
+
+	if (a < 0)
+		b = -(b);
+	return (b);
+}
