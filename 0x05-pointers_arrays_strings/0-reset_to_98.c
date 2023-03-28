df9179d12891d76aa7cd863b3c50e13934d28925
Eze Onyedikachi <thekacchi1@gmail.com>
2023-03-27 17:09:34 -0700

    lol

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - function resets a pointer
+  *
+  * @n: - parameter to be operated on
+  *
+  * Returns: always 0
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+void reset_to_98(int *n);
+
+#endif
