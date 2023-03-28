d2c2b66386db676261c8f15b87014cd4fd20955c
Eze Onyedikachi <thekacchi1@gmail.com>
2023-03-28 04:20:08 -0700

    lol

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_rev - prints string in reverse
+  * @s: parameter to be operated
+  * Return: always 0
+  */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	for (i = count ; i >= 0 ; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
