d9ed3bbcad22767b4775b9c97eca5a4cd729ac5b
Eze Onyedikachi <thekacchi1@gmail.com>
2023-04-03 16:59:18 -0700

    lol

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,21 +4,21 @@
 /**
   *_strpbrk - main entey point
   *@s: parameter to be operated
-  *accept: parameter to be operated
+  *@accept: parameter to be operated
   *Return: Always 0
   */
 
-char *_strpbrk(char *s, char*accept)
+char *_strpbrk(char *s, char *accept)
 {
 	int i, n;
 
-	for(i = 0 ; s[i] != '\0' ; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 	{
-		for(n = 0 ; accept[n] != '\0' ; n++)
+		for (n = 0 ; accept[n] != '\0' ; n++)
 		{
-			if(s[i] == accept[n])
-				return(s + i);
+			if (s[i] == accept[n])
+				return (s + i);
 		}
 	}
-	return(NULL);
+	return (NULL);
 }
