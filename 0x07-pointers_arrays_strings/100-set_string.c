8272bf2b8641fe03e30e3948ffe762de12ff24d9
Eze Onyedikachi <thekacchi1@gmail.com>
2023-04-04 17:36:58 -0700

    lol

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *set_string -main entry point
+  *@s: parameter to be operated
+  *@to: parameter to be operated
+  *Return: always 0
+  */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
