ec1d84684161503ba11f8f9b0421cd4cddadfcd3
Eze Onyedikachi <thekacchi1@gmail.com>
2023-04-17 22:29:15 -0700

    lol

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,18 +2,16 @@
 #include <string.h>
 
 /**
-  *_print_rev_recursion - main entry point
+ *_print_rev_recursion - main entry point
   *@s: parameter to be operted on
   *return: always 0
   */
 
 void _print_rev_recursion(char *s)
 {
-	int lenghtt, i;
-
-	lenghtt = strlen(s);
-	for (i = lenghtt - 1; i >= 0; i--)
+	if (*s)
 	{
-		printf("%c", s[i]);
+		_print_rev_recursion(s + 1);
+		printf("%c", *s);
 	}
 }
