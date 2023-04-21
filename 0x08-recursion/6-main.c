95571c80f402d5cc9d29603d69df2457d691b13c
Eze Onyedikachi <thekacchi1@gmail.com>
2023-04-21 04:28:53 -0700

    lol

--- 0x08-recursion/6-main.c
+++ 0x08-recursion/6-main.c
@@ -1,4 +1,4 @@
-#include main.h
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -10,13 +10,21 @@ int main(void)
 {
     int r;
 
-    r = is_palindrome(level);
-    printf(%dn, r);
-    r = is_palindrome(redder);
-    printf(%dn, r);
-    r = is_palindrome(test);
-    printf(%dn, r);
-    r = is_palindrome(step on no pets);
-    printf(%dn, r);
+    r = is_prime_number(1);
+    printf("%d\n", r);
+    r = is_prime_number(1024);
+    printf("%d\n", r);
+    r = is_prime_number(16);
+    printf("%d\n", r);
+    r = is_prime_number(17);
+    printf("%d\n", r);
+    r = is_prime_number(25);
+    printf("%d\n", r);
+    r = is_prime_number(-1);
+    printf("%d\n", r);
+    r = is_prime_number(113);
+    printf("%d\n", r);
+    r = is_prime_number(7919);
+    printf("%d\n", r);
     return (0);
 }
