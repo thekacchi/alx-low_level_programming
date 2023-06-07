70d5126b2bdaf53bd582384d31d39a237109a452
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-07 15:15:31 -0700

    lol

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - main entry point
+ * @s: parameter to be operated
+ * Return: returns length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int lenghtt;
+
+	lenghtt = strlen(s);
+
+	return (lenghtt);
+}
