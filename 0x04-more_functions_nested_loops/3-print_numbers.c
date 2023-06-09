c664ddbbc5680b6f878b4622574c11f50ad72a8a
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-09 13:01:58 -0700

    kiok

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_numbers - prints series of numbers
+ */
+
 void print_numbers(void)
 {
 	int a;
-	
-	for (a = 0; a <= 9; a++)
 	{
 		_putchar(a + '0');
 		_putchar('\n');
