4cc2ae7a050930432005b4f9f3d030299c66455a
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-09 12:30:10 -0700

    kiloshe

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: parameter
+ * @b: parameter
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
