66385d9d1a87060d84849f38a653856b601031c7
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-10 16:51:12 -0700

    lola

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
