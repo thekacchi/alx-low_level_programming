603585d09732c376f49b0adc655387cd8610bfac
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-14 18:57:26 -0700

    lolade

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * free_grid - main entry point
+ * @grid: operation parameter
+ * @height: operation parameter
+ * Return: Theres no return, void function here
+ */
+
 void free_grid(int **grid, int height)
 {
 	int a;
