592db8e5f76e8f91b6331620111eb2532c95ab0c
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-20 08:15:42 -0700

    lol

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/*
- * main - main entry point
+/**
+ * main - entry point of the program
+ *
  * Return: Always retruns 0
  */
 
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
