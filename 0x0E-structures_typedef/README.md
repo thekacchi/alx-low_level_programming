44530d76868aa9139e5c8722cc27392dbaeb7313
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-20 11:44:33 -0700

    lolade

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+tructt and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOGGY
+#define DOGGY
+
+/**
+ * struct dog - struct for dog prof
+ * @name: name of dog
+ * @age: how old are you
+ * @owner: na who get you
+ *
+ * Description: Struct for some damn dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
