6646b137ce7afefade5dca4b30e08fcef2e904a0
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-21 19:57:23 -0700

    lol

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_name -function prints nname
+ * @name: func parameter
+ * @f: func parameter
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL)
