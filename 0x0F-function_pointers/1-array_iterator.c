6e779df22758de908c03cd518ed6371aa6a31814
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-21 20:21:32 -0700

    lolo

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stddef.h>
 
+/**
+ * array_iterator - prints elements of an array
+ * @array: function para
+ * @size: function para
+ * @action: function para
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	if (array == NULL || action == NULL)
+		return;
+
 	size_t i;
+
 	for (i = 0 ; i < size ; i++)
 	{
 		action(array[i]);
