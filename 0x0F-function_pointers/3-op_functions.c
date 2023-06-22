05e5161ca89672eab12eb4bba0722ec9fa5b3588
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-21 22:22:44 -0700

    gods hand

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - function adds
+ * @a: paramete
+ * @b: parameter
+ * Return: always 0
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_subtract - func sub
+ * @a: para
+ * @b: oara
+ * Return: always 0
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: para
+ * @b: para
+ * Return: alwayz 0
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: para
+ * @b: para
+ * Return: always 0
+ */
+
+int op_div(int a,int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: para
+ * @b: para
+ * Return: always 0
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
