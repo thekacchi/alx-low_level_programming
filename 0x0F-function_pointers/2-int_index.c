21b9e56aaf8f046d36d8b22a4467d99186b7d78e
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-21 20:53:28 -0700

    kolade

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
+/**
+ * int_index - searches for a number
+ * @array: parameter to function
+ * @size: function parameter
+ * @cmp: function parameter
+ * Return: Always 0
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (array == NULL || cmp == NULL || size <= 0)
-		return -1;
+		return (-1);
 
 	for (i = 0; i < size ; ++i)
 	{
-		if(cmp(array[i]) != 0)
-			return i;
-		
+		if (cmp(array[i]) != 0)
+			return (i);
+
 	}
 
-	return -1;
+	return (-1);
 }
