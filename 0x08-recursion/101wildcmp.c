4e21d048b743d7c801b31b7d0cd1aece5b26c061
Onyedikachi Eze <thekacchi1@gmail.com>
2023-06-22 20:31:01 -0700

    kilie

---
+++ 0x08-recursion/101wildcmp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * wildcmp - compare strings
+ * @s1: point erjsbdjbd
+ * @s2: pointer to strint oarameter
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
