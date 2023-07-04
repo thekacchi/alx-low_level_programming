a619d7f09f4c3a951b1147db7e5b30464967452f
Eze Onyedikachi <thekacchi1@gmail.com>
2023-07-03 20:01:12 -0700

    lord

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+ * add_node - adds new node
+ * @head: parameter
+ * @str: string to be added
+ * Return: head
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
