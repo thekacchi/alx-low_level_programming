abaa966123795e9c2e5b376696a1b6068cadcd5e
Eze Onyedikachi <thekacchi1@gmail.com>
2023-07-05 17:23:55 -0700

    lol

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a listsint_t list
+ * @head: points to head of the list
+ *
+ * Desc: starts from the head ant iterates to free memory allocated
+ * to each node till it reaches end of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+	*head = (*head)->next;
+		free(temp);
+	}
+
+	*head = NULL;
+}
