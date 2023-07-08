933a70568253f1d6331a9d7ab0cde90404bc9c28
Eze Onyedikachi <thekacchi1@gmail.com>
2023-07-07 19:36:42 -0700

    lol

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,11 +7,11 @@
  * Return: 1 for success, -1 for fail
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
 
-	*n = (~(1UL << index) | *n);
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
