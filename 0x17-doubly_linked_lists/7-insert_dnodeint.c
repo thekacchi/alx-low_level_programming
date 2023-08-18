b5e74626232fc329e01fa4a04003a878827e6313
Onyedikachi Emmanuel <thekacchi1@gmail.com>
2023-08-17 19:10:36 -0700

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,20 +18,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-	
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
 	new_node->n = n;
-
 	if (idx == 0)
 	{
 		new_node->next = *h;
 		new_node->prev = NULL;
 		if (*h != NULL)
 			(*h)->prev = new_node;
-
 		*h = new_node;
 		return (new_node);
 	}
