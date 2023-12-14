08523ff4977b2dd1288521feaae2ebee020ba8a8
Onyedikachi Eze <thekacchi1@gmail.com>
2023-12-14 00:20:37 -0800

    omo

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -26,7 +26,7 @@ int jump_search(int *array, size_t size, int value)
 		prev += step;
 		if (prev >= size)
 			break;
-		printf("Value checked array [%lu] = [%d]\n", prev, array[prev]);
+/**		printf("Value checked array [%lu] = [%d]\n", prev, array[prev]); */
 	}
 	start = prev - step;
 	printf("Value found between indexes [%lu] and [%lu]\n", start, prev);
